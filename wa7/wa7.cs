02da31ad881f8f92d50b979d9bdce062de5e18a7
bxokhxnhle <khanh.le@uwaterloo.ca>
2021-07-17 12:04:44 -0400

    adding files

---
+++ wa7/wa7.cs
@@ -0,0 +1,157 @@
+#nullable enable
+
+using System;
+using static System.Console;
+
+namespace Bme121
+{
+    static class Program
+    {
+        // Field 'rGen' and methods NewRandomArray and WriteArrayOneLine are used to
+        // generate and display arrays used to test searching and sorting algorithms.
+
+        static Random rGen = new Random( );
+
+        // Create an integer array filled with random values.
+
+        static int[ ] NewRandomArray( int size ) { return NewRandomArray( size, size ); }
+
+        static int[ ] NewRandomArray( int size, int range )
+        {
+            if( size < 1 ) return new int[ 0 ];
+
+            int[ ] result = new int[ size ];
+
+            for( int i = 0; i < result.Length; i ++ )
+            {
+                result[ i ] = rGen.Next( 0, range );
+            }
+
+            return result;
+        }
+
+        // Write up to 10 elements of an integer array, all on one line.
+        // The name to display for the index is passed as a string.
+        // The name to display for the array is passed as a string.
+
+        static void WriteArrayOneLine( int[ ] a ) { WriteArrayOneLine( a, "i", nameof( a ), true ); }
+        static void WriteArrayOneLine( int[ ] a, string indexName, string arrayName, bool showIndex = true )
+        {
+            if( a == null ) return;
+            if( string.IsNullOrEmpty( indexName ) ) indexName = "i";
+            if( string.IsNullOrEmpty( arrayName ) ) arrayName = "a";
+            int nameLength = Math.Max( indexName.Length, arrayName.Length );
+            string fmt = "{0," + nameLength + "}:";
+
+            if( showIndex )
+            {
+                if( a.Length >  0 ) Write( fmt, indexName );
+                if( a.Length >  0 ) Write( $" {0,8}" );
+                if( a.Length >  1 ) Write( $" {1,8}" );
+                if( a.Length >  2 ) Write( $" {2,8}" );
+                if( a.Length >  3 ) Write( $" {3,8}" );
+                if( a.Length >  4 ) Write( $" {4,8}" );
+                if( a.Length > 10 ) Write( " ..." );
+                if( a.Length >  9 ) Write( $" {(a.Length - 5),8}" );
+                if( a.Length >  8 ) Write( $" {(a.Length - 4),8}" );
+                if( a.Length >  7 ) Write( $" {(a.Length - 3),8}" );
+                if( a.Length >  6 ) Write( $" {(a.Length - 2),8}" );
+                if( a.Length >  5 ) Write( $" {(a.Length - 1),8}" );
+                WriteLine( );
+            }
+
+            if( a.Length >  0 ) Write( fmt, arrayName );
+            if( a.Length >  0 ) Write( $" {a[ 0 ],8}" );
+            if( a.Length >  1 ) Write( $" {a[ 1 ],8}" );
+            if( a.Length >  2 ) Write( $" {a[ 2 ],8}" );
+            if( a.Length >  3 ) Write( $" {a[ 3 ],8}" );
+            if( a.Length >  4 ) Write( $" {a[ 4 ],8}" );
+            if( a.Length > 10 ) Write( " ..." );
+            if( a.Length >  9 ) Write( $" {a[ a.Length - 5 ],8}" );
+            if( a.Length >  8 ) Write( $" {a[ a.Length - 4 ],8}" );
+            if( a.Length >  7 ) Write( $" {a[ a.Length - 3 ],8}" );
+            if( a.Length >  6 ) Write( $" {a[ a.Length - 2 ],8}" );
+            if( a.Length >  5 ) Write( $" {a[ a.Length - 1 ],8}" );
+            WriteLine( );
+        }
+
+        // Method SelectSort is used to sort an array of integers.
+
+        static void SelectSort( int[ ] a, out int[ ] swaps )
+        {
+            swaps = new int[0];
+            
+            if( a == null ) return;
+            if( a.Length < 2 ) return;
+            
+            swaps = new int[a.Length - 1];
+            
+            for( int firstUnsorted = 0; firstUnsorted < a.Length - 1; firstUnsorted ++ )
+            {
+                int min = firstUnsorted;
+                for( int i = firstUnsorted + 1; i < a.Length; i ++ )
+                {
+                    if( Compare( a[ i ], a[ min ] ) < 0 ) min = i;
+                }
+                int temp = a[ min ];
+                a[ min ] = a[ firstUnsorted ];
+                a[ firstUnsorted ] = temp;
+                // assign the position of the swaps
+                swaps[firstUnsorted] = min;
+            }
+        }
+        
+        // Method UndoSelectSort receives an array and the swaps used in sorting it
+        // by selection sort. It returns the array to its original unsorted order.
+        
+        static void UndoSelectSort(int[] a, int[] swaps )
+        {
+            if (a == null) throw new ArgumentNullException(nameof(a));
+            if (a.Length < 1) throw new ArgumentOutOfRangeException(nameof(a));
+            
+            for (int i = swaps.Length - 1; i >= 0; i--)
+            {
+                int idTemp = swaps[i];
+                int temp = a[idTemp];
+                a[idTemp] = a[i];
+                a[i] = temp;
+            }
+        }
+
+        // Method Main is used to test selection sorting and its undoing.
+
+        static void Main( )
+        {
+            int[ ] data = NewRandomArray( size: 10, range: 100 );
+            
+            WriteLine( );
+            WriteLine( "Original array" );
+            WriteArrayOneLine( data, "i", nameof( data ) );
+            
+            SelectSort( data, out int[] arrayOfSwaps );
+            
+            WriteLine();
+            WriteLine("Exchanges array");
+            WriteArrayOneLine(arrayOfSwaps, "i", nameof(arrayOfSwaps));
+            
+            WriteLine( );
+            WriteLine( "Sorted array" );
+            WriteArrayOneLine( data, "i", nameof( data ) );
+            
+            UndoSelectSort(data, arrayOfSwaps );
+            
+            WriteLine();
+            WriteLine("Unsorted array");
+            WriteArrayOneLine(data, "i", nameof(data));
+            
+            WriteLine( );
+        }
+
+        // Method Compare is the comparison used in sorting.
+        
+        static int Compare( int a, int b )
+        {
+            return a.CompareTo( b );
+        }
+    }
+}
